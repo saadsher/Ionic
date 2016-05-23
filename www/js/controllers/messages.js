0ec3508cb4b5c7c36d49adc8f33a8b64e6e2df42
Saad Sher <saadshers@gmail.com>
2016-05-23 22:53:59 +0100

    cleaning messages

--- www/js/controllers/messages.js
+++ www/js/controllers/messages.js
@@ -3,12 +3,6 @@ Spasey.controller('MessagesCtrl', function($scope, MessagingService, MessagesPol
     $scope.messages = messages;
   });
 
-  // fix this
-  $scope.expandText = function() {
-  	var element = document.getElementById("sendMsg");
-  	element.style.height =  element.scrollHeight + "px";
-  };
-
   $scope.send = function() {
     return MessagingService.create($scope.now.message).then(function(resolve) {
       console.log('#TODO: we need some loaders, and confirmation message on failure and success');
