6dc214bfb46a2e9d664c919409ab40259b438858
Artur Malecki <a.malecki@livelinktechnology.net>
2016-05-30 20:17:27 +0100

    Inbox feature part 1

--- www/js/services/messaging_service.js
+++ www/js/services/messaging_service.js
@@ -8,6 +8,20 @@ Spasey.service('MessagingService', function($http) {
     },
     getAll: function() {
       return $http.get(host + "/messages")
+    },
+    concierge: {
+      create: function(data) {
+        return $http.post(
+          host + "/concierge/users/" + data.userId + "/messages",
+          { message: { content: data.content } }
+        )
+      },
+      getConversations: function() {
+        return $http.get(host + "/concierge/conversations")
+      },
+      getAll: function(userId) {
+        return $http.get(host + "/concierge/users/" + userId + "/messages")
+      }
     }
   }
 });
